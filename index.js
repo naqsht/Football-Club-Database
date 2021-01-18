89a5d0c04ab87583afd658bc9aaffeb54f4689d3
naqsht <naqsh_thind@sfu.ca>
2021-01-17 16:44:18 -0800

    ejs done

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:@localhost/people'
+});
+
+var app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
